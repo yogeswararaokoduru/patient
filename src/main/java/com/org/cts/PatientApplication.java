9d0587f628046ae4472e0db4cebabb7c8ef402f1
yogeswararaokoduru <yogikkoduru@gmail.com>
2022-09-23 18:07:08 +0530

    h2-console

--- src/main/java/com/org/cts/PatientApplication.java
+++ src/main/java/com/org/cts/PatientApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PatientApplication {
-
+//http://localhost:8080/h2-console
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
 	}
